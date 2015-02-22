d98abc3c88f00a3ab1d010a78ec34f9e19446cb4
Valerian <kinyock.va@gmail.com>
2015-02-22 22:18:36 +0100

    add quicksort, modify readme

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 improving my algorithms skills
 - machine-DP: solving maximization problem using dynamic programming
     ./machines filename
+- quicksort
---
+++ sort/quicksort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.4
+
+def quicksort(liste):
+    
+    if liste == []:
+        return []
+
+    pivot = liste.pop(0)    
+    bigger = list(filter(lambda x: x > pivot, liste))
+    smaller = list(filter(lambda x: x < pivot, liste))
+
+    return (quicksort(smaller) + [pivot] + quicksort(bigger))
+
