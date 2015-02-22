c3ec116dff8ce481fc8e8d42ac38002392892235
Valerian <kinyock.va@gmail.com>
2015-02-22 18:48:27 +0100

    update extract_data, main

--- machines-DP/machines.py
+++ machines-DP/machines.py
@@ -2,15 +2,12 @@
 import re
 from itertools import takewhile
 
-""" author: KINYOCK Valérian 
-"""
 memoization = {}
 
-def maxProfit(dollars, day, hold):
+def maxProfit(dollars, day, hold, max_day, input):
     """ Function using dynamic programming 
     States: selling, buying, selling & buying,skipping (getting profit of the machine), or doing nothing 
     """
-
     # all actions
     sell_buy, sell, buy, skip, nothing = 0, 0, 0, 0, 0
     
@@ -26,20 +23,20 @@ def maxProfit(dollars, day, hold):
     
     # action of buying a machine
     if dollars >= input[day][1] and hold == -1:
-        buy = maxProfit(dollars - input[day][1], day + 1, day)
+        buy = maxProfit(dollars - input[day][1], day + 1, day, max_day, input)
     
     # action of doing nothing (if holding no machine)
     if hold == -1:
-        nothing = maxProfit(dollars, day + 1, hold)
+        nothing = maxProfit(dollars, day + 1, hold, max_day, input)
     
     # if we hold a machine, we can sell, sell&buy or only get the profit of the day (skip)
     elif hold != -1:
-        sell = maxProfit(dollars + input[hold][2], day + 1, -1)
-        skip = maxProfit(dollars + input[hold][3], day + 1, hold)
+        sell = maxProfit(dollars + input[hold][2], day + 1, -1, max_day, input)
+        skip = maxProfit(dollars + input[hold][3], day + 1, hold, max_day, input)
         
         # we can sell&buy only if we have enough money to buy the machine at the current day
         if dollars + input[hold][2] >= input[day][1]:
-            sell_buy = maxProfit(dollars + input[hold][2] - input[day][1], day + 1, day)
+            sell_buy = maxProfit(dollars + input[hold][2] - input[day][1], day + 1, day, max_day, input)
 
     tmp_dollars = max(buy, nothing, sell, skip, sell_buy)
     memoization[dollars, day, hold] = tmp_dollars
@@ -59,7 +56,7 @@ def extract_data(filename):
     for i, val in enumerate(data_lst):
         if val == (0, 0, 0): break
         if len(val) == 3:
-            print(val)        
+                   
             data[val] = (list(takewhile(lambda x: len(x) > 3, data_lst[i+1:])))
             data_dict = { d[0]: (d[0], d[1], d[2], d[3]) for d in data[val] }
             data[val] = []
@@ -72,14 +69,9 @@ def extract_data(filename):
 
 if __name__ == "__main__":
     extracted_data = extract_data("input.txt")
-    """print(fst_line, data)
-    max_day = fst_line[2]
-    input = data
-    print(maxProfit(fst_line[1], 0, -1))
-    
-    print("fst_line:", fst_line)
-    print("data:", data)"""
 
-    for key, val in extracted_data.items():
-        print(key, val)
+    for k, v in extracted_data.items():
+        print(maxProfit(k[1], 0, -1, k[2], v))
     
+
+
