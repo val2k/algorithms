7a13cd439b87559a24a25a902e4a2f164414c55b
Valerian <kinyock.va@gmail.com>
2015-02-22 23:22:20 +0100

    fix

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -7,7 +7,7 @@ def quicksort(liste):
 
     pivot = liste.pop(0)    
     bigger = list(filter(lambda x: x > pivot, liste))
-    smaller = list(filter(lambda x: x < pivot, liste))
+    smaller = list(filter(lambda x: x <= pivot, liste))
 
     return (quicksort(smaller) + [pivot] + quicksort(bigger))
 
